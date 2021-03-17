b51cc560bbb2fd64e3577a18950a7dfb9330b345
ccsxw6 <ccsxw6@gmail.com>
2021-03-17 13:00:33 -0500

    added html routes

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-// need to include express, mongoose, morgan
-
 const express = require('express')
-var morgan = require('morgan')
 const mongoose = require('mongoose');
+var morgan = require('morgan')
 
 const app = express()
-// telling web server what port to listen to
-// listen to whatever is in the environment variable OR 8080 if nothing in env variable
-//process.env global variable is injected by Node at runtime for your application to use, it represents the state of the system environment your application is in when it starts
 const PORT = process.env.PORT || 8080;
 
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
 
 
+require("./routes/htmlRoutes.js")(app)
+
+
+
+// add error here? 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
